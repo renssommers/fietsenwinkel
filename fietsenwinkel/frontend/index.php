398437a5232523983a26e036d304ba07f3c7f287
madeliefraben <madeliefraben52@gmail.com>
2019-06-08 17:21:34 +0200

    opnieuw acties gefixt

--- fietsenwinkel/frontend/index.php
+++ fietsenwinkel/frontend/index.php
@@ -37,37 +37,7 @@
     </head>
     <body>
     
-        <?php
-
-        session_start();
-
-        if (isset($_POST["add"])){
-            if (isset($_SESSION["cart"])){
-                $item_array_id = array_column($_SESSION["cart"],"product_id");
-                if (!in_array($_GET["id"],$item_array_id)){
-                    $count = count($_SESSION["cart"]);
-                    $item_array = array(
-                        'product_id' => $_GET["id"],
-                        'item_name' => $_POST["hidden_name"],
-                        'product_prijs' => $_POST["hidden_price"],
-                        'item_quantity' => $_POST["quantity"],
-                    );
-                    $_SESSION["cart"][$count] = $item_array;
-                    echo '<script>window.location="Cart.php"</script>';
-                }else{
-                    echo '<script>alert("Product is already Added to Cart")</script>';
-                }
-            }else{
-                $item_array = array(
-                    'product_id' => $_GET["id"],
-                    'item_name' => $_POST["hidden_name"],
-                    'product_prijs' => $_POST["hidden_price"],
-                    'item_quantity' => $_POST["quantity"],
-                );
-                $_SESSION["cart"][0] = $item_array;
-            }
-        }
-         include 'header.php' ?>
+        <?php include 'header.php' ?>
         
         <!--================Slider Area =================-->
         <section class="main_slider_area">
@@ -258,23 +228,35 @@
                 <div class="s_m_title">
                     <h2>Aanbiedingen:</h2>
                 </div>
+
                 <div class="l_product_slider owl-carousel">
-
-                    <div class="item">
-                        <div class="l_product_item">
-                            <div class="l_p_img">
-                                <img src="img/product/Damesfiets1.jpg" alt="">
-                            </div>
-                            <div class="l_p_text">
-                                <ul>
-                                        <li><a class="add_cart_btn" href="#">In winkelwagen</a></li>
-                                </ul>
-                                <h4>Dames fiets model 1</h4>
-                                <h5><del>€219,99</del>  €180</h5>
-                            </div>
-                        </div>
-                    </div>
-
+                    <?php 
+                    include 'databasecon.php';
+                    $conn = Opencon();
+                    $QUERY = "SELECT * FROM producten WHERE actie = 1";
+                    $result = mysqli_query($conn, $QUERY);
+                     
+                            while ($row = mysqli_fetch_assoc($result)){
+                                 
+                                  ?>
+                                  <div class="item">
+                                <div class="l_product_item">
+                                    <a class="l_p_img" href="product-details.php?id=<?php echo $row["product_id"]; ?>">
+                                        <img src=<?php echo $row["product_fotos"]; ?> alt="">
+                                      
+                                    </a>
+                                    <div class="l_p_text">
+                                       <ul>
+                                            <li><a class="add_cart_btn" href="#">In winkelwagen</a></li>
+                                        </ul>
+                                        <h4><?php echo $row["product_naam"]; ?></h4>
+                                        <h5><b style="color:#FF9029; font-weight: lighter; padding-right: 5px;">Actieprijs: </b> €<?php echo $row["product_prijs"]; ?></h5>
+                                    </div>
+                                </div>
+                                  </div>
+                    <?php
+                     }
+                    ?>
                 </div>
             </div>
     </section>
@@ -290,39 +272,30 @@
 
                     <div class="l_product_slider owl-carousel">
                     
-
-                    
-                    <?php 
-                      include 'databasecon.php';
-                      $conn = Opencon();
-                      $QUERY = "SELECT * FROM producten ORDER BY `datum_toegevoegd` DESC LIMIT 10";
-                      $result = mysqli_query($conn, $QUERY);
-                      
-                            while ($row = mysqli_fetch_assoc($result)){
-                                    ?>
-                                <div class="item">
-                                     <div class="l_product_item">
-                                        <form method="post" action="shopping-cart2.php?action=add&id=<?php echo $row["product_id"]; ?>">
+                            <?php 
+                            $QUERY = "SELECT * FROM producten ORDER BY `datum_toegevoegd` DESC LIMIT 10";
+                            $result = mysqli_query($conn, $QUERY);
+                            
+                                    while ($row = mysqli_fetch_assoc($result)){
+                                          ?>
+                                          <div class="item">
+                                        <div class="l_product_item">
                                             <a class="l_p_img" href="product-details.php?id=<?php echo $row["product_id"]; ?>">
-                                            <img src=<?php echo $row["product_fotos"]; ?> alt="">
+                                                <img src=<?php echo $row["product_fotos"]; ?> alt="">
+                                              
                                             </a>
                                             <div class="l_p_text">
-                                                <ul>
-                                                <input type="submit" name="add" style="margin-top: 5px;" class="add_cart_btn"
-                                                value="In winkelwagen">
+                                               <ul>
+                                                    <li><a class="add_cart_btn" href="#">In winkelwagen</a></li>
                                                 </ul>
                                                 <h4><?php echo $row["product_naam"]; ?></h4>
                                                 <h5><del></del>  €<?php echo $row["product_prijs"]; ?></h5>
-                                                
-                                                <input type="hidden" name="hidden_name" value="<?php echo $row["product_naam"]; ?>">
-                                                <input type="hidden" name="hidden_price" value="<?php echo $row["product_prijs"]; ?>">
                                             </div>
-                                        </form>
-                                    </div>
-                                </div>
-                            <?php
-                            }
-                            ?>
+                                        </div>
+                                          </div>
+                                    <?php
+                                    }
+                                    ?>
                         </div>
                       
 
