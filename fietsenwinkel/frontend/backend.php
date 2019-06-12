8c5ea2479e976d184fdb17142824a19e471e0824
madeliefraben <madeliefraben52@gmail.com>
2019-06-12 10:01:47 +0200

    begin tabel klanten

--- fietsenwinkel/frontend/backend.php
+++ fietsenwinkel/frontend/backend.php
@@ -35,6 +35,25 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         <![endif]-->
+
+        <style>
+            table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+            }
+
+            td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+            }
+
+            tr:nth-child(even) {
+            background-color: #dddddd;
+            }
+        </style>
+
     </head>
     <body>
         
@@ -103,6 +122,48 @@
                                 <a class="abonneer_btn" href="index.php">Uitloggen</a>
 
                             </div>
+                        </div>
+
+                        <div class="col-lg-9 float-left">
+                            
+                        <table>
+                                    <tr>
+                                        <th>Company</th>
+                                        <th>Contact</th>
+                                        <th>Country</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Alfreds Futterkiste</td>
+                                        <td>Maria Anders</td>
+                                        <td>Germany</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Centro comercial Moctezuma</td>
+                                        <td>Francisco Chang</td>
+                                        <td>Mexico</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Ernst Handel</td>
+                                        <td>Roland Mendel</td>
+                                        <td>Austria</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Island Trading</td>
+                                        <td>Helen Bennett</td>
+                                        <td>UK</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Laughing Bacchus Winecellars</td>
+                                        <td>Yoshi Tannamuri</td>
+                                        <td>Canada</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Magazzini Alimentari Riuniti</td>
+                                        <td>Giovanni Rovelli</td>
+                                        <td>Italy</td>
+                                    </tr>
+                                </table>
+
                         </div>
                     </div>
              
